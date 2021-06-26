212f61f6c9ed76ad63218a5bb67f8bf6b0e3a528
takibek <65259468+takibek@users.noreply.github.com>
2021-06-26 23:16:45 +0100

    Add files via upload

---
+++ car.php
@@ -0,0 +1,21 @@
+<?php
+$hostname="localhost";
+        $username="root";
+        $passowrd="";
+        $dbname="new test";
+        $con=mysqli_connect($hostname,$username,$passowrd,$dbname);
+        $query="select *from car";
+        $result=mysqli_query($con,$query);
+        
+header('Content-Type:text/html; charset=utf-8');
+
+$stu=array();
+while( $row=mysqli_fetch_array($result))
+{
+    $stu[]=$row;
+}
+print "{car:".json_encode($stu,JSON_UNESCAPED_UNICODE)."}";
+
+?>
+
+    
